8ce5950c6d04a9784e16beed9793bde0adaee1f5
Stream Picker <cry.termed@gmail.com>
2026-05-10 10:24:05 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,275 @@
+// ZENITH AI - Klucze licencyjne (Pełna Baza)
+const keyStore = {
+    "1D": [
+        "ZENITH-I05U-H87A-V29V", "ZENITH-KT2K-TPHN-J997", "ZENITH-3NRU-AIMC-AIU3", "ZENITH-HKD1-DKO6-KRBH", "ZENITH-945J-ML07-WLH9",
+        "ZENITH-BDRD-TES0-9YXH", "ZENITH-OZ59-TOTT-98RP", "ZENITH-ESKT-HB78-1RTZ", "ZENITH-QXJQ-CX4O-BXBV", "ZENITH-T7FG-7FV8-08U3",
+        "ZENITH-82YL-VJ13-SBFQ", "ZENITH-TT6C-5O4H-L2R7", "ZENITH-7DTQ-Z1GT-Q1CC", "ZENITH-QTDO-6OUZ-GXV3", "ZENITH-491V-6KG2-KX39",
+        "ZENITH-EXDH-RZM8-PX8T", "ZENITH-0CWF-O4FI-40XK", "ZENITH-ARYF-3QEW-O2OA", "ZENITH-SZP3-S41U-7QI4", "ZENITH-AQXH-KO44-EK3Y",
+        "ZENITH-S7OG-E257-ONAF", "ZENITH-BG3L-G9UQ-1F51", "ZENITH-NOXO-QNJO-7KPQ", "ZENITH-72M8-2AAR-PL0K", "ZENITH-6HRD-NOOH-WJSP",
+        "ZENITH-AUJX-0DZW-GSN4", "ZENITH-RHAO-75F1-8GSO", "ZENITH-BBA3-VBYG-DH2G", "ZENITH-SLR0-ZXXY-LKVF", "ZENITH-76QV-QIBE-Y6XT",
+        "ZENITH-CV2C-GYYI-H1EP", "ZENITH-STVJ-WFUN-4DLU", "ZENITH-3GPF-W7KV-WLJ4", "ZENITH-GJO6-WUUE-3O75", "ZENITH-X3FW-A922-57AH",
+        "ZENITH-32AA-KJQI-HC4J", "ZENITH-Q3U3-ZTR8-S8EM", "ZENITH-TJMT-0S76-XQV7", "ZENITH-19ND-31OW-67P1", "ZENITH-YOZW-TYJ5-X8M3",
+        "ZENITH-S7KJ-5OYE-5TBZ", "ZENITH-S8AB-04OP-NF9R", "ZENITH-SO3X-CPGN-JSDD", "ZENITH-SQHZ-ZPRL-P662", "ZENITH-OUUG-JAL6-8GYJ",
+        "ZENITH-8A56-67WX-E8XB", "ZENITH-QWB7-AS8S-81WA", "ZENITH-W8ZT-SVZX-JBQL", "ZENITH-TX0B-EEHB-CWHP", "ZENITH-93ZB-2X2W-4VWE",
+        "ZENITH-MNJE-6RG1-JW5I", "ZENITH-IDSS-B3P5-8P36", "ZENITH-FW1D-6863-K9W2", "ZENITH-FXTI-WRO4-VFA3", "ZENITH-ZZ7B-0GT1-L74Y",
+        "ZENITH-CF6D-7GO6-ITMX", "ZENITH-UZ1G-AZ17-M4Q3", "ZENITH-QH1V-DW3W-R59V", "ZENITH-HUAP-3DIN-B2IW", "ZENITH-6BCA-YKM4-MBU8",
+        "ZENITH-CDF0-CCT6-MRMQ", "ZENITH-YIOA-4RWE-PHKQ", "ZENITH-BW2N-5SJE-OPBG", "ZENITH-S5YY-DCM8-WB59", "ZENITH-CG7G-OOL2-AZ9A",
+        "ZENITH-UHZ4-8EDS-6OP3", "ZENITH-7AYJ-J18Q-R98O", "ZENITH-RC4N-1SGJ-ESGQ", "ZENITH-EMWT-XWRF-ZIGK", "ZENITH-RTBO-BQTI-GWPB",
+        "ZENITH-3KHY-PRYQ-5JVJ", "ZENITH-UJS6-YRUS-NFON", "ZENITH-H45Q-U98K-TPBB", "ZENITH-IURY-XKAV-R1T2", "ZENITH-CILA-GMXX-UB4V",
+        "ZENITH-WWRX-O9KU-NI3Q", "ZENITH-JHYC-UOI2-AW7H", "ZENITH-TKU9-1EDB-646Q", "ZENITH-3WGO-RFCQ-QAGJ", "ZENITH-RGL6-ZGYP-GXW7",
+        "ZENITH-YGA8-6L10-5GUS", "ZENITH-KMA9-G4KO-ZD1Z", "ZENITH-QS6E-BGMC-YJYK", "ZENITH-EIQU-O0GP-GMGE", "ZENITH-1PLB-MELL-JGCJ",
+        "ZENITH-6G0M-NH28-DANB", "ZENITH-J003-VEQ1-531C", "ZENITH-VW8A-9OXB-TJFY", "ZENITH-V1FD-5Q4H-SM9R", "ZENITH-CTYZ-GTDW-LN56",
+        "ZENITH-OGIL-4XN3-VJGN", "ZENITH-E33Q-X3IY-N0DH", "ZENITH-2FRX-IY2O-XFB6", "ZENITH-DE5K-YCTU-K9N3", "ZENITH-32YE-2B0C-3KQ6",
+        "ZENITH-LAMJ-AA1A-C1OW", "ZENITH-XA3V-9YRJ-1RBD", "ZENITH-PY3A-A352-ZT16", "ZENITH-XSBL-IW15-VZ29", "ZENITH-QZRM-VK4J-BGCU"
+    ],
+    "7D": [
+        "ZENITH-6168-TEOT-PAA1", "ZENITH-W5OP-R6MI-BPMW", "ZENITH-XUHD-403Q-TOJH", "ZENITH-DNEZ-LG6E-A0A2", "ZENITH-9AKX-VUPU-APSF",
+        "ZENITH-2UZW-JLGA-WVJP", "ZENITH-H748-EMAX-7H7B", "ZENITH-Y168-NA45-IMRS", "ZENITH-9OYS-QMRD-043E", "ZENITH-8WYF-M1Q0-PX3G",
+        "ZENITH-7R8O-OWZL-QQEZ", "ZENITH-QTPH-7K2S-087D", "ZENITH-6UU2-4KI4-B4LF", "ZENITH-3GQY-PYPG-ZTK1", "ZENITH-JQU7-3VI0-BGL0",
+        "ZENITH-GNV5-S8G3-59I6", "ZENITH-I9BC-S41L-NTZ3", "ZENITH-16PY-3FPP-NORU", "ZENITH-3LQS-S7Y1-ELAR", "ZENITH-2EI0-PTF5-Q7P9",
+        "ZENITH-GDYN-10ID-BM26", "ZENITH-VWEG-JQ2D-77Z1", "ZENITH-RXSK-NIKU-S7AS", "ZENITH-DVIA-HIZL-K0ZW", "ZENITH-0EKL-6RJP-WOIB",
+        "ZENITH-PW1K-QVSE-0RLI", "ZENITH-41VI-4BIP-CD2C", "ZENITH-YUVK-DJMT-2EUO", "ZENITH-X0GO-AXYH-4FRA", "ZENITH-H10S-VTWK-TRCW",
+        "ZENITH-B0AL-WDAS-4M01", "ZENITH-4S5M-ZIU1-6RE5", "ZENITH-PC5R-ZE8X-3W6Q", "ZENITH-TXUS-1PKM-HMJW", "ZENITH-D6P7-5JC5-OW53",
+        "ZENITH-4ON1-PTHL-ID5K", "ZENITH-CI0W-BUF4-SK3H", "ZENITH-FZDT-EJMO-1MV6", "ZENITH-YEBU-Y27I-Z53R", "ZENITH-JZSR-NR3X-U71G",
+        "ZENITH-JIFJ-VEJ3-5ZLE", "ZENITH-RC6H-DSVB-V2TH", "ZENITH-W48J-8QPP-0VJL", "ZENITH-AZY4-GHKA-1NSV", "ZENITH-TK78-HKKE-EUZ0",
+        "ZENITH-NOY6-KAE1-QNZ4", "ZENITH-2V0X-ZAEV-7UHL", "ZENITH-57M2-R1H2-SFLS", "ZENITH-553I-5D1R-BOO8", "ZENITH-WFWZ-K5H3-6SZO",
+        "ZENITH-N5IL-YN7O-I6N7", "ZENITH-CNV6-Q7ER-4ZEM", "ZENITH-DZCC-QWTZ-0138", "ZENITH-E2ZM-ANRJ-GBW7", "ZENITH-31VR-G29L-RSGU",
+        "ZENITH-AGNJ-HO6K-1JKR", "ZENITH-3XGG-PO45-LK88", "ZENITH-EV51-RQJB-QVLB", "ZENITH-ZD2I-9UQC-STRA", "ZENITH-5OGU-3YK1-1PNA",
+        "ZENITH-HGN5-BW3N-PCNS", "ZENITH-3C68-L6Y1-CI91", "ZENITH-6XK8-JTY3-J8Q7", "ZENITH-JW0Y-Y1SZ-ZMSY", "ZENITH-5NSY-L879-5GQZ",
+        "ZENITH-TWTT-9PQ1-SCWK", "ZENITH-UVW0-EF0C-S2ZR", "ZENITH-C4TY-YPEM-AQSP", "ZENITH-K0RI-D0ZI-7L6M", "ZENITH-VTNF-NXWR-6WTF",
+        "ZENITH-HHLU-UZBB-EBU9", "ZENITH-HSJW-K6XL-ICSQ", "ZENITH-I3K1-PCQP-VM1Z", "ZENITH-NBJG-ZC1V-7V9Y", "ZENITH-Y630-1D1I-N1WB",
+        "ZENITH-JO37-BK97-TXL3", "ZENITH-SJQ9-5OV0-5YFP", "ZENITH-WYV9-2AXL-2T0Q", "ZENITH-ND7C-8VUE-XG0A", "ZENITH-WEP7-7VVG-V71E",
+        "ZENITH-KAXG-I6HW-GQL4", "ZENITH-125A-VQGO-DSZA", "ZENITH-UZFT-A5UR-8ZJ0", "ZENITH-53DA-7EOV-U88J", "ZENITH-HKH9-RRNW-83ZM",
+        "ZENITH-70HA-9V8E-DI3K", "ZENITH-STI1-1FN6-157H", "ZENITH-NXQG-HNJC-3QI7", "ZENITH-3IXM-J2A4-1CQ1", "ZENITH-9IVG-T9SS-S0Y4",
+        "ZENITH-6OZT-1GN6-QLOT", "ZENITH-ANYT-RS2T-DYFG", "ZENITH-I97N-Y2EP-NFH8", "ZENITH-TMQ3-404X-SJYI", "ZENITH-ZM41-94ER-Z0DJ",
+        "ZENITH-JGMN-QE4F-U9P9", "ZENITH-AMYB-LPEA-JVEC", "ZENITH-MAPJ-OQLW-VVFG", "ZENITH-TNIC-PRSE-U4I2", "ZENITH-78PP-19TB-KVP2"
+    ],
+    "1Y": [
+        "ZENITH-86ZI-RFHU-VI22", "ZENITH-1LVY-L9ZS-15F2", "ZENITH-3IQU-XAEB-SYD0", "ZENITH-3FQR-IQFL-XIOB", "ZENITH-OEXL-94XH-KPE4",
+        "ZENITH-YSXY-OGAW-WY10", "ZENITH-HB67-5TJK-9A3D", "ZENITH-ULG0-KIH2-CXQB", "ZENITH-C2X1-XM06-VIQF", "ZENITH-8S8E-M50S-4SK8",
+        "ZENITH-L88P-SQOK-HEAE", "ZENITH-EGTV-JLKH-I8JH", "ZENITH-IRGC-1EPC-48PX", "ZENITH-XG0P-RFQN-C2M6", "ZENITH-Y68Q-3KLW-HX7H",
+        "ZENITH-29CY-QFC0-1JDD", "ZENITH-8JSY-RJXI-H09Y", "ZENITH-PKHU-FC4N-814T", "ZENITH-TTLZ-T916-FC2G", "ZENITH-TY5O-WFOU-Y6PG",
+        "ZENITH-V4P0-ZOXC-40PV", "ZENITH-WDYZ-YY27-MFC0", "ZENITH-RSP2-P753-494H", "ZENITH-XFFH-5E75-MD51", "ZENITH-SOCE-1SY4-WCVN",
+        "ZENITH-9R1S-QO7J-W2KY", "ZENITH-4A80-KC93-87R1", "ZENITH-T8S3-NFXQ-0YZV", "ZENITH-06YU-5S5R-KC92", "ZENITH-BBVU-EKLB-LRJE",
+        "ZENITH-Z02X-01VP-O8WD", "ZENITH-0AAP-988F-OUUW", "ZENITH-27MG-X6XZ-NFA7", "ZENITH-RIYC-KQKQ-JBBM", "ZENITH-HFBY-MDUJ-V4JI",
+        "ZENITH-BBWB-1NLE-5AJ0", "ZENITH-0OZC-PP29-982C", "ZENITH-EPAA-PB0O-02OS", "ZENITH-HJRN-5O63-5ZKZ", "ZENITH-3ZAM-NQ9W-YEO4",
+        "ZENITH-YFAZ-P6IT-H5YW", "ZENITH-B7PY-JAYS-DZ5O", "ZENITH-DW1F-Q0A3-540R", "ZENITH-ABSM-2470-BHTA", "ZENITH-7CXC-UZW9-B2J5",
+        "ZENITH-K4QQ-Q4XE-M02D", "ZENITH-0WYQ-3T4E-04UL", "ZENITH-738W-JFKP-EUAN", "ZENITH-VWWU-RJM5-2CE1", "ZENITH-2AZ4-UHA7-XIB2",
+        "ZENITH-AO1U-QGLE-0XEE", "ZENITH-QZKJ-Z848-VO93", "ZENITH-TFOU-8FNS-LT11", "ZENITH-R70K-K3KK-ILMX", "ZENITH-RXQG-2BLZ-HNO5",
+        "ZENITH-V4TN-51PX-SUSZ", "ZENITH-G7QN-9VJM-UW94", "ZENITH-L2C1-DN51-SUQP", "ZENITH-Z9T8-IELJ-0OR4", "ZENITH-41U4-KHI5-JVU9",
+        "ZENITH-K8V1-LBG7-OZAE", "ZENITH-37E2-HJHK-JB5T", "ZENITH-DKBK-LR92-KKSG", "ZENITH-LSL2-9SA9-O7P8", "ZENITH-CYRZ-AIEM-6R7U",
+        "ZENITH-W9H9-WRDY-Z0TK", "ZENITH-2KTV-1Z8M-FTNO", "ZENITH-KWH0-K27F-ELFF", "ZENITH-HJGT-8UP6-03Q0", "ZENITH-S0BV-9NT3-Q41L",
+        "ZENITH-MJIX-JCJD-ELTW", "ZENITH-31DG-2SLE-67QU", "ZENITH-6DG9-MU19-303Z", "ZENITH-5QZG-Y6VC-LF9Z", "ZENITH-2J04-5OV2-JVBC",
+        "ZENITH-NT2U-PCXJ-5KFS", "ZENITH-2364-HOKB-5W4H", "ZENITH-VR0Y-R5RB-DW2P", "ZENITH-QQS1-MG4G-86W3", "ZENITH-SMW6-SW2L-31UB",
+        "ZENITH-SI7C-DHUD-LLLD", "ZENITH-NEXJ-V4BA-OD58", "ZENITH-L2CF-1M8D-GYP7", "ZENITH-Z4ZM-EAXL-FB1D", "ZENITH-MM13-ULFP-1FCC",
+        "ZENITH-94OU-TY73-H89Z", "ZENITH-EKPX-2HJ2-8M9M", "ZENITH-Q0LR-5E4S-DTPM", "ZENITH-6Z80-21F5-FNL0", "ZENITH-Z2A4-RV2B-34SK",
+        "ZENITH-Y418-SVCG-LNJN", "ZENITH-VWTY-FHJ0-31EK", "ZENITH-BKIK-AHXC-841J", "ZENITH-WLJY-ML18-FCHP", "ZENITH-BLWL-R6JJ-N16D",
+        "ZENITH-X4TW-F4P9-UEV2", "ZENITH-OHO9-8PJC-6JS1", "ZENITH-I833-UF4W-8JFJ", "ZENITH-517K-EFYE-MEZ1", "ZENITH-42PA-M9AS-25LP"
+    ],
+    "LT": [
+        "ZENITH-E4TR-BKO3-BDP0", "ZENITH-P06B-AZKA-A11N", "ZENITH-NP1L-BEHP-GAM3", "ZENITH-DE12-9UVO-T5CE", "ZENITH-6848-D7TB-161K",
+        "ZENITH-92DW-UXJS-8B7C", "ZENITH-TPSU-TCIS-OZT3", "ZENITH-3SDS-PSQA-ZLTA", "ZENITH-DO5R-6I62-M7LP", "ZENITH-CBFS-9T2Z-5G8M",
+        "ZENITH-3612-EX37-JSSC", "ZENITH-S71I-4M8R-S9LB", "ZENITH-XQHH-6BDP-BDZT", "ZENITH-V8GY-S7KX-R668", "ZENITH-UPHX-TNZ0-KNAN",
+        "ZENITH-1P1E-E0DN-FZG5", "ZENITH-U4M5-333A-HFAJ", "ZENITH-4JNF-O9QY-G5WJ", "ZENITH-DBLY-CY1O-4TBK", "ZENITH-U9DT-UECN-1A4U",
+        "ZENITH-VOQE-U69T-ECX1", "ZENITH-M1NB-Z0A9-X0MW", "ZENITH-EBKY-P9MJ-SMTL", "ZENITH-H24Z-3JWA-I0K3", "ZENITH-NVOV-IBX5-8RDS",
+        "ZENITH-W7M8-6Z4Q-JYD4", "ZENITH-TT6C-412N-ECPE", "ZENITH-6OOA-8O1W-CJH1", "ZENITH-CBO4-NN5A-JHGN", "ZENITH-QFVF-GBOB-EUDR",
+        "ZENITH-UM8I-7WCC-8X4W", "ZENITH-MAOP-3ACV-ZA1C", "ZENITH-VTEU-0OUX-8T8S", "ZENITH-S22B-C5A7-GEMT", "ZENITH-O1E0-Q55U-I4XO",
+        "ZENITH-SA85-TD2K-TTE1", "ZENITH-F5XA-BCY1-JLAE", "ZENITH-PQFB-EP7W-AWL8", "ZENITH-QBEN-PKH2-3MVN", "ZENITH-VBNZ-LJ3A-Y6O4",
+        "ZENITH-FCLQ-K4OH-UKNV", "ZENITH-0GGT-VAP7-9HRN", "ZENITH-WMDN-K5XJ-ASQE", "ZENITH-PHCK-B3S3-SGBF", "ZENITH-BXXF-EB48-RZ12",
+        "ZENITH-DFZP-QXD0-U26J", "ZENITH-JPOJ-T722-2UYK", "ZENITH-C1LN-71V6-WGZD", "ZENITH-6VX5-6T5D-D6NG", "ZENITH-P7XY-W6OY-BEKI",
+        "ZENITH-48BZ-2IVQ-2AC9", "ZENITH-XDXV-PFBV-SERV", "ZENITH-GWWX-5M7V-L02W", "ZENITH-KU2G-E3QJ-J8RV", "ZENITH-UZSX-VUFC-CQJU",
+        "ZENITH-B4WL-QK1V-EY0M", "ZENITH-4IRZ-XMEM-8246", "ZENITH-AG8T-H702-1FJL", "ZENITH-QN43-WAFG-UAF0", "ZENITH-XPVD-PAYR-2IFB",
+        "ZENITH-9ZZF-2Y3S-N3RH", "ZENITH-9Z0F-JXMK-OQQT", "ZENITH-UOT3-JH0B-BHTY", "ZENITH-KXNW-8A7L-X4I4", "ZENITH-62FS-5MOZ-QNEM",
+        "ZENITH-YV64-C6XN-KXG2", "ZENITH-IXYA-7IGN-7EME", "ZENITH-ISCK-YCZL-IR5W", "ZENITH-CCZW-H3IP-QDGI", "ZENITH-U3PX-36IQ-F52K",
+        "ZENITH-WJCA-UOVO-24K4", "ZENITH-G81U-G5P4-7DP3", "ZENITH-PWK7-AL9B-4QDY", "ZENITH-1IFJ-OFJ2-Y2BJ", "ZENITH-3LJY-Q8P0-C4NR",
+        "ZENITH-26VL-PIBR-E5GS", "ZENITH-QD0R-YBKJ-4B5O", "ZENITH-T19W-MFY9-NGV5", "ZENITH-291R-23AP-Q3W7", "ZENITH-1TCN-HEUN-JHBL",
+        "ZENITH-HMU2-JNL9-J3JM", "ZENITH-K748-7ANH-7MXX", "ZENITH-DKW1-QMNJ-07RZ", "ZENITH-RSTZ-S9EP-HZS0", "ZENITH-Q1LM-CI4X-0IT5",
+        "ZENITH-G2TX-6SPW-2EK9", "ZENITH-CMCM-QGZF-PCSS", "ZENITH-7JWK-S9KR-E0CV", "ZENITH-WNI5-I1OL-UMBI", "ZENITH-WVU9-EXED-9NZK",
+        "ZENITH-DYMG-AGZB-L5YN", "ZENITH-F1T0-CY9S-0TNP", "ZENITH-VCRT-37J9-MOM9", "ZENITH-BU30-O1QP-75D2", "ZENITH-BR05-W7JP-51T6",
+        "ZENITH-MNHA-F971-91XP", "ZENITH-9O2D-9CK2-45F9", "ZENITH-JTYR-IGNW-GIK9", "ZENITH-N8Z1-I7NJ-TQ9R", "ZENITH-U5Y2-K01A-2MZT"
+    ]
+};
+
+const products = [
+    { id: 1, type: "1D", name: "ZENITH AI - 1 Dzień", price: "9.99", icon: "⏱️" },
+    { id: 2, type: "7D", name: "ZENITH AI - 7 Dni", price: "39.99", icon: "📅" },
+    { id: 3, type: "1Y", name: "ZENITH AI - 1 Rok", price: "99.99", icon: "🚀" },
+    { id: 4, type: "LT", name: "ZENITH AI - Lifetime", price: "249.99", icon: "♾️" }
+];
+
+const productGrid = document.getElementById('productGrid');
+const modal = document.getElementById('paymentModal');
+const closeModal = document.querySelector('.close-modal');
+const paymentStatus = document.getElementById('paymentStatus');
+const modalProductName = document.getElementById('modalProductName');
+
+let selectedProduct = null;
+
+function renderProducts() {
+    productGrid.innerHTML = products.map(product => `
+        <div class="product-card">
+            <div class="product-icon">${product.icon}</div>
+            <h3>${product.name}</h3>
+            <div class="price">${product.price} PLN</div>
+            <button class="btn btn-primary buy-btn" onclick="openModal(${product.id})">Kup Teraz</button>
+        </div>
+    `).join('');
+}
+
+function openModal(productId) {
+    selectedProduct = products.find(p => p.id === productId);
+    modal.style.display = 'block';
+    modalProductName.innerText = `Produkt: ${selectedProduct.name} (${selectedProduct.price} PLN)`;
+    paymentStatus.style.display = 'none';
+    document.querySelector('.payment-methods').style.display = 'grid';
+}
+
+closeModal.onclick = () => { modal.style.display = 'none'; };
+window.onclick = (event) => { if (event.target == modal) modal.style.display = 'none'; };
+
+document.querySelectorAll('.pay-btn').forEach(btn => {
+    btn.onclick = function() {
+        const method = this.getAttribute('data-method');
+        showStatus(method);
+    };
+});
+
+function showStatus(method) {
+    const methodsContainer = document.querySelector('.payment-methods');
+    methodsContainer.style.display = 'none';
+    paymentStatus.style.display = 'block';
+    paymentStatus.innerHTML = '<div class="loader">Ładowanie...</div>';
+
+    setTimeout(() => {
+        switch(method) {
+            case 'paypal': renderPayPal(); break;
+            case 'crypto': renderCrypto(); break;
+            case 'psc': renderPSC(); break;
+        }
+    }, 500);
+}
+
+// --- PAYPAL INTEGRATION ---
+function renderPayPal() {
+    paymentStatus.innerHTML = `
+        <div id="paypal-button-container"></div>
+        <p style="font-size: 0.8rem; color: #94a3b8; margin-top: 10px; text-align: center;">Płatność zostanie zweryfikowana automatycznie.</p>
+    `;
+    
+    paypal.Buttons({
+        createOrder: (data, actions) => {
+            return actions.order.create({
+                purchase_units: [{ amount: { value: selectedProduct.price } }]
+            });
+        },
+        onApprove: (data, actions) => {
+            return actions.order.capture().then(details => {
+                completePayment("PayPal - " + details.payer.name.given_name);
+            });
+        }
+    }).render('#paypal-button-container');
+}
+
+// --- CRYPTO INTEGRATION ---
+function renderCrypto() {
+    paymentStatus.innerHTML = `
+        <div style="text-align: left;">
+            <p style="margin-bottom: 10px; font-weight: 600;">Wyślij <span style="color: #f7931a;">${selectedProduct.price} PLN</span> w BTC/LTC:</p>
+            <div style="background: #0f172a; padding: 10px; border-radius: 8px; font-size: 0.7rem; margin-bottom: 15px; border: 1px solid #475569;">
+                <strong>BTC:</strong> bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh<br><br>
+                <strong>LTC:</strong> LUK9pB4m2zTjK1LpX3C2yU9vX5v8v8v8v8
+            </div>
+            <p style="font-size: 0.85rem; margin-bottom: 5px;">Wklej ID Transakcji (TXID):</p>
+            <input type="text" id="txidInput" placeholder="TXID..." style="width: 100%; padding: 10px; border-radius: 8px; background: #0f172a; color: white; border: 1px solid #475569; margin-bottom: 10px;">
+            <button class="btn btn-primary" style="width: 100%;" onclick="verifyCrypto()">Wyślij do weryfikacji</button>
+        </div>
+    `;
+}
+
+// --- DISCORD WEBHOOK CONFIG ---
+const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1502941008537583617/r7I5tO77RWMmemc0faZGsR9tN7TeOHNMb3Qml1GnQrcG7ofVuQMBGf6rqI0ui58m0e00";
+
+function sendToDiscord(method, value) {
+    const data = {
+        embeds: [{
+            title: "🔔 NOWA TRANSAKCJA - ZENITH AI",
+            color: method === 'PSC' ? 15548997 : 16750106,
+            fields: [
+                { name: "Produkt", value: selectedProduct.name, inline: true },
+                { name: "Cena", value: selectedProduct.price + " PLN", inline: true },
+                { name: "Metoda", value: method, inline: true },
+                { name: "Kod/TXID", value: "```" + value + "```" }
+            ],
+            timestamp: new Date()
+        }]
+    };
+    fetch(DISCORD_WEBHOOK, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(data) });
+}
+
+function verifyCrypto() {
+    const txid = document.getElementById('txidInput').value;
+    if (txid.length < 10) { alert("Podaj poprawne ID!"); return; }
+    paymentStatus.innerHTML = '<div class="loader">Wysyłanie...</div>';
+    sendToDiscord("CRYPTO", txid);
+    setTimeout(() => {
+        paymentStatus.innerHTML = `
+            <div style="text-align: center;">
+                <div style="color: #f39c12; font-size: 3rem; margin-bottom: 1rem;">⏳</div>
+                <h3>Wysłano!</h3>
+                <p style="color: #94a3b8;">Administrator sprawdzi TXID. Klucz otrzymasz wkrótce.</p>
+                <button class="btn btn-outline" style="margin-top: 15px;" onclick="location.reload()">Zamknij</button>
+            </div>
+        `;
+    }, 1500);
+}
+
+function renderPSC() {
+    paymentStatus.innerHTML = `
+        <div style="text-align: left;">
+            <p style="margin-bottom: 15px;">Wprowadź kod PIN Paysafecard:</p>
+            <input type="text" id="pscInput" placeholder="XXXX-XXXX-XXXX-XXXX" style="width: 100%; padding: 12px; border-radius: 8px; border: 1px solid #475569; background: #0f172a; color: white; text-align: center; font-size: 1rem; margin-bottom: 15px;">
+            <button class="btn btn-primary" style="width: 100%;" onclick="submitPSC()">Wyślij do weryfikacji</button>
+        </div>
+    `;
+}
+
+function submitPSC() {
+    const psc = document.getElementById('pscInput').value;
+    if (psc.length < 16) { alert("Błędny kod!"); return; }
+    sendToDiscord("PSC", psc);
+    paymentStatus.innerHTML = `
+        <div style="text-align: center;">
+            <div style="color: #3498db; font-size: 3rem; margin-bottom: 1rem;">📩</div>
+            <h3>Wysłano!</h3>
+            <p style="color: #94a3b8;">Kod PSC sprawdzany ręcznie. Czekaj na kontakt (max 15 min).</p>
+            <button class="btn btn-outline" style="margin-top: 15px;" onclick="location.reload()">Zamknij</button>
+        </div>
+    `;
+}
+
+function completePayment(info) {
+    const key = getRandomKey(selectedProduct.type);
+    paymentStatus.innerHTML = `
+        <div style="text-align: center;">
+            <div style="color: #22c55e; font-size: 3rem; margin-bottom: 1rem;">✅</div>
+            <h3>Płatność zatwierdzona!</h3>
+            <p style="color: #94a3b8;">Metoda: ${info}</p>
+            <div style="background: rgba(139, 92, 246, 0.1); border: 1px solid #8b5cf6; padding: 1.5rem; border-radius: 12px; margin: 1.5rem 0;">
+                <div style="font-size: 0.8rem; color: #8b5cf6;">TWÓJ KLUCZ:</div>
+                <div style="font-family: monospace; font-size: 1.2rem; font-weight: 700; color: white;">${key}</div>
+            </div>
+            <a href="zenith_ai.zip" class="btn btn-primary" style="width: 100%;" download>Pobierz .zip</a>
+        </div>
+    `;
+}
+
+function getRandomKey(type) {
+    const keys = keyStore[type];
+    if (!keys || keys.length === 0) return "BRAK KLUCZY - KONTAKTUJ SIĘ Z SUPPORTEM";
+    const idx = Math.floor(Math.random() * keys.length);
+    const key = keys[idx];
+    keys.splice(idx, 1);
+    return key;
+}
+
+renderProducts();
